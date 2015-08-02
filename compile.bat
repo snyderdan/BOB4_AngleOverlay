c8fd6d8e46252a82e56f1de2be2f220471af4fd9
Dan Snyder <snyderdan46@gmail.com>
2015-08-02 15:56:30 -0400

    Added optimization flags, changed to 270 degree tilt

--- compile.bat
+++ compile.bat
@@ -1,13 +1,13 @@
-avr-gcc -mmcu=atmega640 -Wall -Wl,-u,vfprintf -lprintf_flt -c main.c -o main.o
-avr-gcc -mmcu=atmega640 -Wall -Wl,-u,vfprintf -lprintf_flt -lm -c command_handler.c -o command_handler.o
-avr-gcc -mmcu=atmega640 -Wall -c pantilt.c -o pantilt.o
-avr-gcc -mmcu=atmega640 -Wall -c analog.c -o analog.o
+avr-gcc -mmcu=atmega640 -Wall -O3 -Wl,-u,vfprintf -lprintf_flt -c main.c -o main.o
+avr-gcc -mmcu=atmega640 -Wall -O3 -Wl,-u,vfprintf -lprintf_flt -lm -c command_handler.c -o command_handler.o
+avr-gcc -mmcu=atmega640 -Wall -O3 -c pantilt.c -o pantilt.o
+avr-gcc -mmcu=atmega640 -Wall -O3 -c analog.c -o analog.o
 avr-gcc -mmcu=atmega640 -Wall -c usart.c -o usart.o
-avr-gcc -mmcu=atmega640 -Wall -c array.c -o array.o
+avr-gcc -mmcu=atmega640 -Wall -O3 -c array.c -o array.o
 
-avr-gcc -mmcu=atmega640 -gstabs+ -combine pantilt.c main.c command_handler.c usart.c array.c analog.c -o debug_format
+avr-gcc -mmcu=atmega640 -gstabs+ -O3 -combine pantilt.c main.c command_handler.c usart.c array.c analog.c -o debug_format
 
-avr-gcc -mmcu=atmega640 -Wall -Wl,-u,vfprintf -lprintf_flt -lm main.o analog.o usart.o command_handler.o pantilt.o array.o -o gen.elf
+avr-gcc -mmcu=atmega640 -O3 -Wall -Wl,-u,vfprintf -lprintf_flt -lm main.o analog.o usart.o command_handler.o pantilt.o array.o -o gen.elf
 
 avr-objcopy -j .eeprom --set-section-flags=.eeprom="alloc,load" --change-section-lma .eeprom=0 -O ihex gen.elf EEPROM_DATA.eep
 
